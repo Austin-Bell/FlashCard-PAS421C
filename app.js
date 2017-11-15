80a5143795d3199d48e99ebcbdaa552ab29534c9
Austin <austinbell.93@gmail.com>
2017-11-15 15:09:59 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+  $('.flashcard').on('click', function() {
+    $('.flashcard').toggleClass('flipped');
+  });
+});
+$(document).ready(function() {
+  $('.flashcard-1').on('click', function() {
+    $('.flashcard-1').toggleClass('flipped');
+  });
+});
+$(document).ready(function() {
+  $('.flashcard-2').on('click', function() {
+    $('.flashcard-2').toggleClass('flipped');
+  });
+});
+
+$(document).ready(function() {
+  $('.flashcard-3').on('click', function() {
+    $('.flashcard-3').toggleClass('flipped');
+  });
+});
+
+$(document).ready(function() {
+  $('.flashcard-4').on('click', function() {
+    $('.flashcard-4').toggleClass('flipped');
+  });
+});
+
+$(document).ready(function() {
+  $('.flashcard-5').on('click', function() {
+    $('.flashcard-5').toggleClass('flipped');
+  });
+});
+
+$(document).ready(function() {
+  $('.flashcard-6').on('click', function() {
+    $('.flashcard-6').toggleClass('flipped');
+  });
+});
+$(document).ready(function() {
+  $('.flashcard-7').on('click', function() {
+    $('.flashcard-7').toggleClass('flipped');
+  });
+});
+$(document).ready(function() {
+  $('.flashcard-8').on('click', function() {
+    $('.flashcard-8').toggleClass('flipped');
+  });
+});
+$(document).ready(function() {
+  $('.flashcard-9').on('click', function() {
+    $('.flashcard-9').toggleClass('flipped');
+  });
+});
+$(document).ready(function() {
+  $('.flashcard-10').on('click', function() {
+    $('.flashcard-10').toggleClass('flipped');
+  });
+});
